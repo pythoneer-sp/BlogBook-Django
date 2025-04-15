eae3ba1dd23bd5c430fd3c72485a00d6a3a2f5c9
Saurav Kumar <155866156+pythoneer-sp@users.noreply.github.com>
2025-04-15 23:30:31 +0530

    Added templates

--- mainblog/blogbook/urls.py
+++ mainblog/blogbook/urls.py
@@ -2,5 +2,8 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.blog_list, name='blog_list'),
+    path('create/', views.blog_create, name='blog_create'),
+    path('<int:blog_id>/edit/', views.blog_edit, name='blog_edit'),
+    path('<int:blog_id>/delete/', views.blog_delete, name='blog_delete'),
 ]
